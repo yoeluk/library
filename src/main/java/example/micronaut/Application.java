a0cd60da2fac493f52743bc7631f69962a24c9b8
Yoel Garcia Diaz <ygarciadiaz@bluecatnetworks.com>
2022-07-05 20:52:22 -0400

    openapi properties

--- src/main/java/example/micronaut/Application.java
+++ src/main/java/example/micronaut/Application.java
@@ -5,7 +5,7 @@ import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@OpenAPIDefinition(info = @Info(title = "Library API", version = "1.0"))
+@OpenAPIDefinition(info = @Info(title = "${api.title}", version = "${api.version}"))
 public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
